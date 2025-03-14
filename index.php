6e11f9521fcdb6d98c34386fc6a46278c1e3aad4
sanneliis <sanneliis00@gmail.com>
2025-03-14 14:56:57 +0200

    Update index.php
    kehtna khk pilt

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
   <body>
     <h1> Meie projekt ITK24 grupiga</h1>
     <p>See leht arvestab meie muudatustega</p>
+    <img> src=<"https://sonumid.ee/wp-content/uploads/2019/07/Kehtna_1-copy-696x392.jpg">
   </body>
 </html>
